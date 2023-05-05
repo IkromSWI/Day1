9e3bdc025d6f2fe5add5743024cdaf67ef9176cc
IkromSWI <111283447+IkromSWI@users.noreply.github.com>
2023-05-05 16:16:58 +0500

    Initial commit

---
+++ inputs.py
@@ -0,0 +1,6 @@
+# lengeth of name which resived in input method
+name = input("What is your name ?\n")
+print(len(name))
+
+# another way off in the top but this is without variables
+print(len(input("What is your name ?\n")))
---
+++ main.py
@@ -0,0 +1,3 @@
+# concat 2 strings in one print method
+print("hello" + " " + "World")
+
